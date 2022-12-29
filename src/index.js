6c5f14f445201235c82f18cc03e922229ecdac42
Ryhor Volasau <G.volosov@gmail.com>
2022-12-29 15:39:53 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        const x = n * -1;
+        const str = String(x);
+        const arr = Array.from(str).map(Number);
+        const newArr = arr.reverse();
+        const res = newArr.join("");
+        const result = Number(res);
+
+        return result;
+    } else {
+        const str = String(n);
+        const arr = Array.from(str).map(Number);
+        const newArr = arr.reverse();
+        const res = newArr.join("");
+        const result = Number(res);
+
+        return result;
+    }
+};
